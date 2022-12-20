da359ffb79e2554579b5f2132da08c48302a9e17
deniskeller <dentean@mail.ru>
2022-12-20 16:46:20 +0400

    alert

--- src/components/base/BaseAlert/BaseAlert.tsx
+++ src/components/base/BaseAlert/BaseAlert.tsx
@@ -32,6 +32,11 @@ const BaseAlert = () => {
       onMouseLeave={endPause}
     >
       {toasts.map((toast) => {
+        const offset = calculateOffset(toast, {
+          reverseOrder: false,
+          gutter: 8,
+        });
+
         const ref = (el: HTMLDivElement) => {
           if (el && !toast.height) {
             const height = el.getBoundingClientRect().height;
@@ -45,7 +50,10 @@ const BaseAlert = () => {
             key={toast.id}
             ref={ref}
             style={{
+              position: 'absolute',
+              transition: 'all 0.5s ease-out',
               opacity: toast.visible ? 1 : 0,
+              transform: `translateY(${offset}px)`,
             }}
             {...toast.ariaProps}
           >
@@ -69,6 +77,51 @@ const BaseAlert = () => {
       })}
     </div>
   );
+
+  // return (
+  //   <div
+  //     className={styles.BaseAlert}
+  //     onMouseEnter={startPause}
+  //     onMouseLeave={endPause}
+  //   >
+  //     {toasts.map((toast) => {
+  //       const ref = (el: HTMLDivElement) => {
+  //         if (el && !toast.height) {
+  //           const height = el.getBoundingClientRect().height;
+  //           updateHeight(toast.id, height);
+  //         }
+  //       };
+
+  //       return (
+  //         <div
+  //           className={styles.BaseAlert__toast}
+  //           key={toast.id}
+  //           ref={ref}
+  //           style={{
+  //             opacity: toast.visible ? 1 : 0,
+  //           }}
+  //           {...toast.ariaProps}
+  //         >
+  //           <div className={styles.BaseAlert__content}>
+  //             {typeIcon(toast.type)}
+  //             <span>{toast.message}</span>
+  //           </div>
+  //           {
+  //             <BaseIcon
+  //               className={styles.BaseAlert__tick_icon}
+  //               viewBox="0 0 22 22"
+  //               icon={ALL_ICONS.TICK_ICON_LIGHT}
+  //               onClick={() => {
+  //                 toast.visible = false;
+  //                 toastOBj.dismiss(toast.id);
+  //               }}
+  //             />
+  //           }
+  //         </div>
+  //       );
+  //     })}
+  //   </div>
+  // );
 };
 
 export default BaseAlert;
