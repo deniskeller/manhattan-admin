b0c99fd2565abc08f3bb520290f96f7c41ce8e20
deniskeller <dentean@mail.ru>
2022-12-25 22:51:30 +0400

    ref

--- src/components/base/BaseStatus/BaseStatusForm.tsx
+++ src/components/base/BaseStatus/BaseStatusForm.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
-import styles from "./BaseStatus.module.scss";
-import {TStatusFrom} from "../../pages/App/Forms/FormsList/FormsList";
+import styles from './BaseStatus.module.scss';
+
+export type TStatusFrom = 'INCOMING' | 'IN_PROGRESS' | 'COMPLETED';
+
 type Props = {
-  status: TStatusFrom
-}
+  status: TStatusFrom;
+};
 
-const BaseStatusForm:React.FC<Props> = ({status}) =>{
-
+const BaseStatusForm: React.FC<Props> = ({ status }) => {
   const data =
-    status === "INCOMING" ? {color: "rgba(26, 26, 26, 0.3)", text: "Incoming"} :
-      status === "IN_PROGRESS" ? {color:"#8C97D9", text: "In progress"} :
-        status === "COMPLETED" ? {color: "#1AC057", text: "Completed"} :
-          {color: "rgba(26, 26, 26, 0.05)", text: "Unknown"};
-  return(
+    status === 'INCOMING'
+      ? { color: 'rgba(26, 26, 26, 0.3)', text: 'Incoming' }
+      : status === 'IN_PROGRESS'
+      ? { color: '#8C97D9', text: 'In progress' }
+      : status === 'COMPLETED'
+      ? { color: '#1AC057', text: 'Completed' }
+      : { color: 'rgba(26, 26, 26, 0.05)', text: 'Unknown' };
+  return (
     <span className={styles.Status}>
-      <span className={`${styles.Circle}`} style={{backgroundColor: data.color}}/>
-      <span className={styles.Text}>
-           {data.text}
-      </span>
+      <span
+        className={`${styles.Circle}`}
+        style={{ backgroundColor: data.color }}
+      />
+      <span className={styles.Text}>{data.text}</span>
     </span>
-  )
-}
+  );
+};
 
 export default BaseStatusForm;
