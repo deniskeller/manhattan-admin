f7cf540828e60afb6827d77312236b01538a9ade
deniskeller <dentean@mail.ru>
2023-01-09 20:18:13 +0400

    ref popup

---
+++ pages/app/admin/transactions.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Application } from '@layouts/index';
+
+const TransactionsPage = () => {
+  return (
+    <Application>
+      <>
+        <h1>transactions page</h1>
+      </>
+    </Application>
+  );
+};
+
+export default TransactionsPage;
