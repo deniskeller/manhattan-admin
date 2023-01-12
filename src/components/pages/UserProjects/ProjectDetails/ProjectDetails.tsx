f676dad65e42e1008536dc87c94aeb7e7e654376
deniskeller <dentean@mail.ru>
2023-01-12 22:11:20 +0400

    project details header

--- src/components/pages/UserProjects/ProjectDetails/ProjectDetails.tsx
+++ src/components/pages/UserProjects/ProjectDetails/ProjectDetails.tsx
@@ -1,9 +1,10 @@
-import { BaseButtonApp, BaseTitle } from '@base/index';
+import { BaseButtonApp, BaseIcon, BaseTitle } from '@base/index';
 import { useAppDispatch } from '@hooks/redux';
 import { modalSlice } from '@store/modals/reducer';
 import React from 'react';
 import { useRouter } from 'next/router';
 import s from './ProjectDetails.module.scss';
+import { ALL_ICONS } from '@constants/icons';
 
 type Props = {
   id: string;
@@ -34,7 +35,64 @@ const ProjectDetails: React.FC<Props> = ({ id }) => {
           />
         </div>
 
-        <div className={s.ProjectDetails_Details}></div>
+        <div className={s.ProjectDetails_Details}>
+          <div className={s.ProjectDetails_Details_Header}>
+            <div className={s.ProjectDetails_Details_Header_InvestedInfo}>
+              <div
+                className={s.ProjectDetails_Details_Header_InvestedInfo_Graph}
+              >
+                <div className={s.GraphLabels}>
+                  <div
+                    className={`${s.GraphLabels_Item} ${s.GraphLabels_Item_Initial}`}
+                  >
+                    200,000.00
+                  </div>
+                  <span className={s.GraphLabels_Item_Slash}>/</span>
+                  <div
+                    className={`${s.GraphLabels_Item} ${s.GraphLabels_Item_Current}`}
+                  >
+                    120 000
+                  </div>
+                  <div
+                    className={`${s.GraphLabels_Item} ${s.GraphLabels_Item_Total}`}
+                  >
+                    1,500,000.00
+                  </div>
+                </div>
+
+                <div className={s.GraphProgressBar}>
+                  <span className={s.GraphProgressBar_Item}></span>
+                  <span className={s.GraphProgressBar_Item}></span>
+                </div>
+
+                <div className={s.GraphDate}>
+                  <div className={s.GraphDate_Label}>Round ends in</div>
+
+                  <div className={s.GraphDate_Tooltip}>
+                    <BaseIcon
+                      icon={ALL_ICONS.TOOLTIP}
+                      viewBox="0 0 20 20"
+                      className={s.GraphDate_Tooltip_Icon}
+                    />
+
+                    <div className={s.GraphDate_Tooltip_Tooltip}>
+                      <span>31.02.2022</span>&nbsp;—&nbsp;
+                      <span>12.00</span>
+                    </div>
+                  </div>
+
+                  <div className={s.GraphDate_Date}>
+                    <span>42 days 18 hrs 51 min</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className={s.ProjectDetails_Details_Header_Border}></div>
+
+            <div className={s.ProjectDetails_Details_Header_Logo}></div>
+          </div>
+        </div>
 
         <div className={s.ProjectDetails_Slider}></div>
       </div>
